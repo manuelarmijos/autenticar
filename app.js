df16ecad18553e35c5ff43b8126622babe8caa5d
manolo1994 <msarmijiso@gmail.com>
2023-04-13 02:13:33 -0500

    cambios analizador cabezeras

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
 	origin: 'http://localhost:8080',
