217f3ea4668af562ccffc2e19d044c0fb0bfb5a9
manolo1994 <msarmijiso@gmail.com>
2023-04-13 02:29:34 -0500

    cambio parametros

--- controllers/autenticarCliente.js
+++ controllers/autenticarCliente.js
@@ -21,6 +21,7 @@ module.exports = {
 	autenticar(req, res) {
 		console.log('Dentro del recurso para autenticar cliente')
 		console.log(req.body)
+		console.log(req.params)
 		console.log(process.env.SECRET)
 		return autenticarCliente
 			.findAll({
